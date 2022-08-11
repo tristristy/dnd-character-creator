c58e3cf0e5e1bfeabde09a7e41a2a561b258da61
Kelly Wacker <kelwacker@gmail.com>
2022-08-10 22:25:46 -0400

    character creator page

--- assets/script/creatorButtons.js
+++ assets/script/creatorButtons.js
@@ -1,7 +1,30 @@
 const basicInfoEl = document.querySelector('.baseinfo');
+const levelsEl = document.querySelector('.levels');
+const classEl = document.querySelector('.classes');
+const proficienciesEl = document.querySelector('.proficiencies');
+const equipEl = document.querySelector('.equip');
+const spellsEl = document.querySelector('.spells');
+const miscEl = document.querySelector('.miscinfo');
 
-function showBasicInfo() {
-    
-}
+function showBasicInfo() {}
+
+function showLevels() {}
+
+function showClass() {}
+
+function showProficiencies() {}
+
+function showEquip() {}
+
+function showSpells() {}
+
+function showMisc() {}
+
 
 basicInfoEl.addEventListener("click", showBasicInfo);
+levelsEl.addEventListener("click", showLevels);
+classEl.addEventListener("click", showClass);
+proficienciesEl.addEventListener("click", showProficiencies);
+equipEl.addEventListener("click", showEquip);
+spellsEl.addEventListener("click", showSpells);
+miscEl.addEventListener("click", showMisc);
