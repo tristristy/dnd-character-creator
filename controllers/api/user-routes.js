42a8a7f9b03e271a1d64e63fd19e61a43c862503
livmeierx <liv.meier13@gmail.com>
2022-08-16 15:47:52 -0400

    created get routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,36 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//Get all users
+router.get('/', (req, res) => {
+  User.findAll()
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+//Get one user 
+route.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
