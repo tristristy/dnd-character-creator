55cb310f2f387c00db8332623f337d2a550fb164
Kelly Wacker <kelwacker@gmail.com>
2022-08-17 11:09:26 -0400

    updates

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const User = require('./User');
-const Spell = require('./Spell');
+// const Spell = require('./Spell');
 const Player = require('./Player');
-const Background = require('./Background');
-const SkillProf = require('./SkillProf');
-const Race = require('./Tools');
-const Class = require('./Class');
-const Language = require('./Language');
-const Tool = require('./Tools')
+// const Background = require('./Background');
+// const SkillProf = require('./SkillProf');
+// const Race = require('./Tools');
+// const Class = require('./Class');
+// const Language = require('./Language');
+// const Tool = require('./Tools')
 
 User.hasMany(Player, {
   foreignKey: 'user_id'
@@ -16,33 +16,33 @@ Player.belongsTo(User, {
   foreignKey: "user_id"
 });
 
-Player.hasOne(Race, {
-  foreignKey: "race_id"
-});
+// Player.hasOne(Race, {
+//   foreignKey: "race_id"
+// });
 
-Player.hasOne(Class, {
-  foreignKey: "class_id"
-});
+// Player.hasOne(Class, {
+//   foreignKey: "class_id"
+// });
 
-Class.hasMany(Spell, {
-  foreignKey: "class_id"
-});
+// Class.hasMany(Spell, {
+//   foreignKey: "class_id"
+// });
 
-Background.hasMany(Language, {
-  foreignKey: "background_lang"
-});
+// Background.hasMany(Language, {
+//   foreignKey: "background_lang"
+// });
 
-Background.hasMany(Tool, {
-  foreignKey: "background_tool"
-});
+// Background.hasMany(Tool, {
+//   foreignKey: "background_tool"
+// });
 
-Background.hasMany(SkillProf,{
-  foreignKey: "background_prof"
-});
+// Background.hasMany(SkillProf,{
+//   foreignKey: "background_prof"
+// });
 
-Race.hasMany(Language, {
-  foreignKey: "race_id"
-})
+// Race.hasMany(Language, {
+//   foreignKey: "race_id"
+// })
 
 
-module.exports = { User, Player, Race, Language, Tool, Class, Spell, Background};
+module.exports = { User, Player};
