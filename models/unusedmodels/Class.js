420fb5728ca6d87bb0fea441774b45ca3b96571e
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-08-16 20:27:24 -0400

    fixed syntax in player-routes

---
+++ models/unusedmodels/Class.js
@@ -0,0 +1,37 @@
+// const { Model, DataTypes } = require("sequelize");
+// const { model } = require("../config/connection");
+// const sequelize = require("../config/connection");
+
+// class Class extends Model {}
+
+// Class.init(
+//   {
+//     class_id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     class_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     hit_dice: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     features: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "class",
+//   }
+// );
+
+// model.exports = Class;
---
+++ models/unusedmodels/Race.js
@@ -0,0 +1,35 @@
+// const { Model, DataTypes } = require("sequelize");
+
+// const sequelize = require("../config/connection");
+
+// class Race extends Model {}
+
+// Race.init(
+//   {
+//     race_id: {
+//       type: DataType.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     race_name: {
+//       type: DataType.STRING,
+//       allowNull: false,
+//     },
+//     race_language: {
+//       type: DataType.STRING,
+//       allowNull: false,
+//       references: {
+//         model: "language",
+//         key: "id",
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "race",
+//   }
+// );
