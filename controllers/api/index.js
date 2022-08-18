d70bdb8319dd521d09d7dd40fbe6ad139e63d2f7
kwax17 <104600244+kwax17@users.noreply.github.com>
2022-08-17 22:24:10 -0400

    collects user and player to be exported

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
 
-const { route } = require('./api');
-const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const userRoutes = require('./user-routes.js');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
 router.use('/users', userRoutes);
 router.use('/players', playerRoutes);
 
