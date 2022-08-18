587f0abb887ab66823506d9f9249bbeafa34cb44
Scotch <horsinaround235@gmail.com>
2022-08-18 19:35:54 -0400

    merge conflicts resolved

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,12 @@
 const router = require('express').Router();
 
 // const { route } = require('./api');
-<<<<<<< HEAD
 // const apiRoutes = require('./api');
 const playerRoutes = require('./player-routes.js');
 const userRoutes = require('./user-routes.js');
 
 // router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
-=======
-const userRoutes = require('./user-routes');
-const playerRoutes = require('./player-routes');
-
->>>>>>> 0f8aa742ad9b053a008bfc872fbd05a5b0257f0f
 router.use('/users', userRoutes);
 router.use('/players', playerRoutes);
 
